229d5965b09983edbf8dac57001f13c0d5ca550c
Andrew Masih <armasih@outlook.com>
2021-06-29 23:51:55 +0000

    added comments

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ do {
 
 /* do {block code} while (condition): */
 
-/* the do while loop will run the block of code at least once, even though the condition is false, it means that the block of code runs before the condition is checked, the condition comes after the block of code; this loo is used if you want to run the block of code t least once no matter what*/
+/* the do while loop will run the block of code at least once, even though the condition is false, it means that the block of code runs before the condition is checked, the condition comes after the block of code; this loo is used if you want to run the block of code at least once no matter what*/
 
-/* the 'do while' loop will stop iteration when the condidto is false */
+/* the 'do while' loop will stop iteration when the condidtion is false */
